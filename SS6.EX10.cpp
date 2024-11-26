f230876e0577fc1a06bf3107d890b923a3379844
domanh09012006 <doducmanh09012006@gmail.com>
2024-11-26 23:47:45 +0700

    Add files via upload

---
+++ SS6.EX10.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n, i;
+	while(n<100){
+		printf("Nhap so nguyen: ");
+		scanf("%d", &n);
+		if(n<=1){
+			printf("Khong la so nguyen to");
+		}else{
+			if(n&)
+		}
+	
+	
